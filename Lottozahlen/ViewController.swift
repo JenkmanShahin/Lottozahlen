03f50175dea3a5d7c7a7e543358ddea39129d0f6
JenkmanShahin <Jenkmanshahin17@gmail.com>
2022-11-04 09:59:13 +0100

    Initial Commit

---
+++ Lottozahlen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottozahlen
+//
+//  Created by jenkman Shahin on 04.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
